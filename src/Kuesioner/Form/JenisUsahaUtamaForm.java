01ae2308852b43ee8079b52e6b5fb4cd41ac0707
Karina Cindy Rahmanto <75431192+karinacindy40@users.noreply.github.com>
2022-03-26 11:05:42 +0700

    Update JenisUsahaUtamaForm.java

--- src/Kuesioner/Form/JenisUsahaUtamaForm.java
+++ src/Kuesioner/Form/JenisUsahaUtamaForm.java
@@ -58,5 +58,6 @@ public class JenisUsahaUtamaForm extends Form {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from
                                                                        // nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+    //halo
 
 }
